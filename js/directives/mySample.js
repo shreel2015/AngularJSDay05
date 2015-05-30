85e28cff7421880b6c939cdb35261a8371d08fc0
Shreel Desai <shreel@promactinfo.com>
2015-05-30 17:09:43 +0530

    AngularJS Day05 Demo

---
+++ js/directives/mySample.js
@@ -0,0 +1,8 @@
+'use strict';
+
+eventsApp.directive('mySample', function() { 
+	return {
+		restrict: 'C',
+		template: "<input type='text' ng-model='sampleData' class='form-control' /><br>{{sampleData}}"
+	};
+});
